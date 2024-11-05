153432ed44318545c51abaf948e482ed8de40bfe
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-05 14:58:38 +0800

    Add user input prompt

--- script.js
+++ script.js
@@ -4,8 +4,24 @@ const getComputerChoice = () => {
     const choice = Math.floor(Math.random() * 3);
     
     if (choice == 0) return "rock";
-    if (choice == 1) return "paper";
-    if (choice == 2) return "scissors";
+    else if (choice == 1) return "paper";
+    else if (choice == 2) return "scissors";
 };
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+const getHumanChoice = () => {
+    let choice = prompt("Choose between 3 options, [R, P, S]");
+
+    let message = "";
+
+    if (choice.toLowerCase() == "r") message = "rock";
+    else if (choice.toLowerCase() == "p") message = "paper";
+    else if (choice.toLowerCase() == "s") message = "scissors";
+
+    else message = "wrong choice given";
+
+    return console.log(message);
+};
+
+getHumanChoice();
