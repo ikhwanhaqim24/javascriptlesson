e3c86fcdd00eebf0883874d072ba07b2dd863afb
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-05 14:47:19 +0800

    Successfully add random logic for RPS

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 // console.log('Hello World')
 
 const getComputerChoice = () => {
-    const choice = Math.random();
-    console.log(choice);
+    const choice = Math.floor(Math.random() * 3);
+    
+    if (choice == 0) return "Rock";
+    if (choice == 1) return "Paper";
+    if (choice == 2) return "Scissors";
 };
 
-getComputerChoice();
+console.log(getComputerChoice());
